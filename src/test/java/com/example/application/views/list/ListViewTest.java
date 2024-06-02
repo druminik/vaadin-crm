1079c7e18f41724cb1a8408107531481bd37af6f
Dominik Liebmann <dominik_l@gmx.net>
2024-06-02 18:05:32 +0200

    adds tests

---
+++ src/test/java/com/example/application/views/list/ListViewTest.java
@@ -0,0 +1,37 @@
+package com.example.application.views.list;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.application.data.Contact;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.data.provider.ListDataProvider;
+
+@SpringBootTest
+public class ListViewTest {
+
+  @Autowired
+  private ListView listView;
+
+  @Test
+  public void formShownWhenContactSelected() {
+    Grid<Contact> grid = listView.grid;
+    Contact firstContact = getFirstItem(grid);
+
+    ContactForm form = listView.contactForm;
+    assertFalse(form.isVisible());
+    grid.asSingleSelect().setValue(firstContact);
+    assertTrue(form.isVisible());
+    assertEquals(firstContact.getFirstName(), form.firstName.getValue());
+  }
+
+  @SuppressWarnings("unchecked")
+  private Contact getFirstItem(Grid<Contact> grid) {
+    return ((ListDataProvider<Contact>) grid.getDataProvider()).getItems().iterator().next();
+  }
+}
